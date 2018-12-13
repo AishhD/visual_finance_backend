01a215b0ee8594777971f6d0c7082c9c14c6aa08
wimbleclaw <aisha305@hotmail.co.uk>
2018-12-13 13:23:59 +0000

    edited serializer to user, city & age

--- app/serializers/age_option_serializer.rb
+++ app/serializers/age_option_serializer.rb
@@ -1,7 +1,7 @@
 class AgeOptionSerializer < ActiveModel::Serializer
   attributes :id
   has_many :spending_category
-  # class SpendingDatumSerializer < ActiveModel::Serializer
-  #   attributes :id, :food_non_alcholic_drinks, :alcoholic_drinks_tobacco_narcotics, :clothing_footwear, :household_bills, :recreation_culture, :education, :resturants_hotels, :transport, :other
-  # end
+  class SpendingDatumSerializer < ActiveModel::Serializer
+    attributes :id, :food_non_alcholic_drinks, :alcoholic_drinks_tobacco_narcotics, :clothing_footwear, :household_bills, :recreation_culture, :education, :resturants_hotels, :transport, :other
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,11 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password, :age, :location, :children, :spending_datum
+  attributes :id, :username, :password, :age, :location, :children
   has_many :spending_datum
   has_many :target_datum
-  # class SpendingDatumSerializer < ActiveModel::Serializer
-  #   attributes :id, :food_non_alcholic_drinks, :alcoholic_drinks_tobacco_narcotics, :clothing_footwear, :household_bills, :recreation_culture, :education, :resturants_hotels, :transport, :other
-  # end
-  # class TargetDatumSerializer < ActiveModel::Serializer
-  #   attributes :id, :food_non_alcholic_drinks, :alcoholic_drinks_tobacco_narcotics, :clothing_footwear, :household_bills, :recreation_culture, :education, :resturants_hotels, :transport, :other
-  # end
+  class SpendingDatumSerializer < ActiveModel::Serializer
+    attributes :id, :food_non_alcholic_drinks, :alcoholic_drinks_tobacco_narcotics, :clothing_footwear, :household_bills, :recreation_culture, :education, :resturants_hotels, :transport, :other
+  end
+  class TargetDatumSerializer < ActiveModel::Serializer
+    attributes :id, :food_non_alcholic_drinks, :alcoholic_drinks_tobacco_narcotics, :clothing_footwear, :household_bills, :recreation_culture, :education, :resturants_hotels, :transport, :other
+  end
 end
