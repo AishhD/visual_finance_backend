ff6400e812a404c12e1ad844df397c1694b3f8c5
wimbleclaw <aisha305@hotmail.co.uk>
2018-12-21 10:44:08 +0000

    updated national averages

--- app/controllers/api/v1/national_averages_controller.rb
+++ app/controllers/api/v1/national_averages_controller.rb
@@ -1,13 +1,13 @@
-class NationalAverageController < ApplicationController
+class Api::V1::NationalAveragesController < ApplicationController
     def index
-        @national_average = NationalAverage.all
-        render json: @national_average
+        @national_averages = NationalAverage.all
+        render json: @national_averages
     end
   
     def show
        @national_average = NationalAverage.find_by(id: params[:id])
        if @national_average
-           render json: @national_average
+           render json: @national_averages
        else
            render json: {error: "The national average was not found"}, status: 404
        end
--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,7 @@ end
 # not used yet
 namespace :api do
   namespace :v1 do
-    resources :national_average, only: [:index, :show]
+    resources :national_averages, only: [:index, :show]
   end
 end
 
