27d4db47f1ae1c77e90ed46f7a912d57aa0a0b9e
wimbleclaw <aisha305@hotmail.co.uk>
2019-01-03 14:42:15 +0000

    slight issue

--- app/serializers/spending_data_serializer.rb
+++ app/serializers/spending_data_serializer.rb
@@ -1,3 +1,3 @@
 class AgeOptionSerializer < ActiveModel::Serializer
-    attributes :id, :food_non_alcholic_drinks, :alcoholic_drinks_tobacco_narcotics, :clothing_footwear, :household_bills, :recreation_culture, :education, :resturants_hotels, :transport, :other,
-  end
+    attributes :id, :food_non_alcholic_drinks, :alcoholic_drinks_tobacco_narcotics, :clothing_footwear, :household_bills, :recreation_culture, :education, :resturants_hotels, :transport, :other
+end
